f961e1bbe1bfbcf8b12b2aa3ad778027855e2a89
Robbie McKinstry <thesnowmancometh@gmail.com>
2022-08-01 15:45:40 -0400

    Extract core types into module

---
+++ src/core/mod.rs
@@ -0,0 +1,11 @@
+pub use clause::Clause;
+pub use formula::Formula;
+pub use literal::Literal;
+pub use sign::Sign;
+pub use variable::Variable;
+
+mod clause;
+mod formula;
+mod literal;
+mod sign;
+mod variable;
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,19 @@
 #![allow(dead_code)]
 
-pub use clause::Clause;
-pub use formula::Formula;
+pub use crate::core::{Clause, Formula, Literal, Sign, Variable};
+
 pub use frame::Frame;
-pub use literal::Literal;
 pub use opcode::Opcode;
 pub use parser::Parser;
-pub use sign::Sign;
 pub use solver::Solver;
 pub use status::Status;
 pub use summary::Summary;
-pub use variable::Variable;
 
 mod autoincrementer;
-mod clause;
-mod formula;
+mod core;
 mod frame;
-mod literal;
 mod opcode;
 mod parser;
-mod sign;
 mod solver;
 mod status;
 mod summary;
-mod variable;
