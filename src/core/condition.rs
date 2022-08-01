cb68718debcddb3becffaaac0c4a428c9cbb97dd
Robbie McKinstry <thesnowmancometh@gmail.com>
2022-08-01 16:32:56 -0400

    Add Condition type to logically separate conditioning from literals

---
+++ src/core/condition.rs
@@ -0,0 +1,24 @@
+#[derive(Clone, Copy)]
+pub enum TruthAssignment {
+    True,
+    False,
+}
+
+/// A `Condition` is a truth-assignment to a `Variable`.
+/// It's logically distinct from a Literal, which is a
+/// `Variable` found in a `Clause` with a given polarity.
+#[derive(Clone, Copy)]
+pub struct Condition {
+    id: Variable,
+    assignment: TruthAssignment,
+}
+
+impl Condition {
+    #[must_use]
+    pub fn new(variable: Variable, assignment: TruthAssignment) -> Self {
+        Self {
+            id: variable,
+            assignment,
+        }
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,10 +1,12 @@
 pub use clause::Clause;
+pub use condition::Condition;
 pub use formula::Formula;
 pub use literal::Literal;
 pub use sign::Sign;
 pub use variable::Variable;
 
 mod clause;
+mod condition;
 mod formula;
 mod literal;
 mod sign;
